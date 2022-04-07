8b6b21a2b96b0a401bc7853997c4650173b31565
Davis Peck <Dpeck1997@gmail.com>
2022-04-07 13:02:19 -0700

    adding the index into the frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,9 @@ import axios from "axios";
     data: function () {
       return {
         message: "Welcome to Vue.js you filthy animals",
-        fruits: []
+        fruits: [],
+        newFruits: {},
+        currentFruits: {}
       };
     },
     created: function () {
@@ -14,22 +16,38 @@ import axios from "axios";
       indexFruits: function () {
         axios.get("/fruits").then((response) => {
           console.log("fruits index", response);
-          this.fruits = response.data
+          this.fruits = response.data;
         });
       },
+      showFruits: function (fruit) {
+        console.log('show')
+        document.querySelector("#fruit-details").showModal();
+        this.currentFruit = fruit;
+      }
     },
   };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <h2>All Fruits</h2>
-    <div v-for="fruit in fruits" v-bind:key="fruit.id">
-    <h2>{{ fruit.name }}</h2>
-    </div>
+
+  <button v-on:click="showFruit(fruit)">More Info</button>
+
+  <h1>{{ message }}</h1>
+  <h2>All Fruits</h2>
+  <br><br>
+  <div v-for="fruit in fruits" v-bind:key="fruit.id">
+  <h2>{{ fruit.name }}</h2>
+  <img v-bind:src="fruit.image">
+  <p>Color: {{ fruit.color }}</p>
+  <br>
+  </div>
 
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 30%;
+}
+</style>
